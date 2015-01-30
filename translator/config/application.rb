763b90a769ed5f719718591a5cb1aad9d588082d
kubkowski <jakub.opara@gmail.com>
2015-01-30 13:52:45 +0100

    Added translations routes and cabybara integration tests

--- translator/config/application.rb
+++ translator/config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Translator
   class Application < Rails::Application
+    # Set translator backend for I18n
+    require "translator"
+    config.i18n.backend = Translator::Backend.new
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- translator/test/test_helper.rb
+++ translator/test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara'
+require 'capybara/rails'
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+    include Capybara::DSL
+    include Rails.application.routes.url_helpers
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
